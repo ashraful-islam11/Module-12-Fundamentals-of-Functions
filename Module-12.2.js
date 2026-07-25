80c8cf46df426da6d4ff86caa2005bc0ddf490f5
Ashraful Islam <ashraful171790@gmail.com>
2026-07-25 10:08:43 +0600

    Function intro with animation

---
+++ Module-12.2.js
@@ -0,0 +1,16 @@
+//todo:   Module no -12. 2          topic: Function Intro with animation.
+
+
+// animated video function: 
+
+function macherTorkari (mase, mosla, sobji){
+    console.log(' তেল গরম করুন ');
+    console.log('  মসলা ফোরান ');
+    console.log('  মাছ দিন ');
+    console.log('  সবজি দিন');
+    
+    return 'সুস্বাদু খাবার প্রস্তুত';
+}
+
+const result = macherTorkari('ইলিশ', 'মসলা', 'আলু')
+console.log(result);
